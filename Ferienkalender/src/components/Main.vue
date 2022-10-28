17f8c4112ddd8e25abe5c0110a473d3a368d2b21
Justin <justin@kauz.ch>
2022-10-29 00:44:57 +0200

    Reason selector

--- Ferienkalender/src/components/Main.vue
+++ Ferienkalender/src/components/Main.vue
@@ -6,7 +6,7 @@
           <div class="w-full overflow-hiddenpy-4">
             <div class="w-full py-2 px-4 ">
               <div class="border-black border-2 rounded-sm">
-                <p class="text-center bg-[#427CAC] rounded-t-sm">Am/Ab</p>
+                <p class="text-center bg-[#427CAC] rounded-t-sm">Am/Ab*</p>
                 <input class="p-2 rounded-b-sm w-full" type="date" required>
               </div>
             </div>
@@ -17,13 +17,21 @@
               </div>
             </div>
           </div>
-          <div class="px-4 py-2">
+          <div class="w-full px-4 py-2">
             <input class=" inline-block m-2" type="checkbox" @click="showUntilInput = !showUntilInput" />
             <p class="inline-block">Halber Tag abwesend</p>
           </div>
+          <div class="w-full px-4 py-2">
+            <div class="border-black border-2 rounded-sm">
+              <p class="text-center bg-[#427CAC] rounded-t-sm w-full">Grund*</p>
+              <select class="w-full minimal">
+                <option>--Auswählen--</option>
+              </select>
+            </div>
+          </div>
           <div class="flex w-full px-4 ">
             <div class="border-black border-2 rounded-sm w-full">
-              <p class=" bg-[#427CAC] rounded-t-sm text-center">Kommentar</p>
+              <p class=" bg-[#427CAC] rounded-t-sm text-center">Beschreibung</p>
               <textarea class="p-2 rounded-b-sm w-full max-h-[10rem]" name="" id="" rows="3"></textarea>
             </div>
           </div>
@@ -120,5 +128,63 @@ export default {
 </script>
 
 <style scoped>
+/* https://www.sliderrevolution.com/resources/css-select-styles/ */
+select {
+  /* styling */
+  background-color: white;
+  border: thin solid blue;
+  font: inherit;
+  line-height: 1.5em;
+  padding: 0.5em 3.5em 0.5em 1em;
 
+  /* reset */
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+}
+
+
+/* arrows */
+
+select.minimal {
+  background-image:
+    linear-gradient(45deg, transparent 50%, gray 50%),
+    linear-gradient(135deg, gray 50%, transparent 50%),
+    linear-gradient(to right, #ccc, #ccc);
+  background-position:
+    calc(100% - 20px) calc(1em + 2px),
+    calc(100% - 15px) calc(1em + 2px),
+    calc(100% - 2.5em) 0.5em;
+  background-size:
+    5px 5px,
+    5px 5px,
+    1px 1.5em;
+  background-repeat: no-repeat;
+}
+
+select.minimal:focus {
+  background-image:
+    linear-gradient(45deg, green 50%, transparent 50%),
+    linear-gradient(135deg, transparent 50%, green 50%),
+    linear-gradient(to right, #ccc, #ccc);
+  background-position:
+    calc(100% - 15px) 1em,
+    calc(100% - 20px) 1em,
+    calc(100% - 2.5em) 0.5em;
+  background-size:
+    5px 5px,
+    5px 5px,
+    1px 1.5em;
+  background-repeat: no-repeat;
+  border-color: green;
+  outline: 0;
+}
+
+
+select:-moz-focusring {
+  color: transparent;
+  text-shadow: 0 0 0 #000;
+}
 </style>
