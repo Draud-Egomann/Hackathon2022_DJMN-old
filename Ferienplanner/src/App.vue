e1705bc5c5f32a837ea9474dc3e6c93d31846786
xxAinzxx <dominic.tosku@gmail.com>
2022-10-28 18:13:05 +0200

    Changed Home

--- Ferienplanner/src/App.vue
+++ Ferienplanner/src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Calendar from './components/Calendar.vue'
 </script>
 
 <template>
-  <HelloWorld/>
+  <Calendar/>
 </template>
 
 <style scoped>
