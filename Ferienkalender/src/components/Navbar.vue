973a195c55323b0fe3900bc2acb70f459e6e4977
Justin <justin@kauz.ch>
2022-10-28 18:59:33 +0200

    logo addded

--- Ferienkalender/src/components/Navbar.vue
+++ Ferienkalender/src/components/Navbar.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <section class="w-full mx-auto">
-      <div class="relative bg-gray-200 p-4">
+      <div class="relative bg-gray-200 p-2">
+        <img src="../assets/logo.jpg" alt="Projekt Ferienplanner" class="w-64 hidden lg:inline" />
+        <h3 class="inline lg:hidden">Projekt Ferienplanner</h3>
         <router-link to="/" class="btn-Header m-2 inline-block text-lg">Kalendar
         </router-link>
         <router-link to="/" class="btn-Header m-2 inline-block text-lg">Statistik
