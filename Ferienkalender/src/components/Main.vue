dafdc123aff20867de2890dafebc1276a0396649
xxAinzxx <dominic.tosku@gmail.com>
2022-10-28 20:44:51 +0200

    test calendar

--- Ferienkalender/src/components/Main.vue
+++ Ferienkalender/src/components/Main.vue
@@ -23,14 +23,24 @@ export default {
           dayGridDay: {
             type: 'dayGridDay',
           }
-        }
+        },
+        navLinks: true,
+        selectable: true,
+        dateClick: this.changeToDay
       },
+    }
+  },
+  methods: {
+    changeToDay: function(info:any) {
+      console.log("Clicked", info);
+      let calendarApi = this.$refs.fullCalendar.getApi()
+      calendarApi.next()
     }
   }
 }
 </script>
 <template>
   <div class="w-1/2 m-auto">
-    <FullCalendar :options="calendarOptions" />
+    <FullCalendar ref="fullCalendar" :options="calendarOptions" />
   </div>
 </template>
