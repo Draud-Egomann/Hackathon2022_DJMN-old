23038b744af0470f6be4c23ad5d5a25b7ee718b8
Justin <justin@kauz.ch>
2022-10-28 22:05:18 +0200

    input vacation beta

--- Ferienkalender/src/components/Main.vue
+++ Ferienkalender/src/components/Main.vue
@@ -1,6 +1,50 @@
 <template>
-  <div class="w-1/2 m-auto">
-    <FullCalendar :options="calendarOptions" />
+  <div>
+    <div class="bg-gray-300 w-3/4 mx-auto my-4 rounded-md">
+      <div class="flex flex-wrap overflow-hidden">
+        <!-- check days -->
+        <form class="flex flex-wrap w-full overflow-hidden lg:w-1/2 py-4">
+          <div class="flex flex-wrap w-full overflow-hiddenpy-4">
+            <div class="w-1/2 px-4 ">
+              <div class="border-black border-2 rounded-md">
+                <p class="text-center bg-gray-400 rounded-t-md">Am/Ab</p>
+                <input class="p-2 rounded-b-md w-full" type="date" required>
+              </div>
+            </div>
+            <div v-if="showUntilInput" class="w-1/2 px-4 ">
+              <div class="border-black border-2 rounded-md">
+                <p class="text-center bg-gray-400 rounded-t-md">Bis (optional)</p>
+                <input class="p-2 rounded-b-md w-full" type="date" required>
+              </div>
+            </div>
+            <!-- <button class="btn" type="submit" value="submit">Bestätigen</button> -->
+          </div>
+          <div class="flex flex-wrap w-full">
+            <div class="w-1/2 p-2">
+              <input class=" inline-block m-2" type="checkbox" @click="showUntilInput = !showUntilInput" />
+              <p class="inline-block">Halber Tag abwesend</p>
+            </div>
+          </div>
+          <div class="flex w-full px-4 ">
+            <div class="border-black border-2 rounded-md w-full">
+              <p class=" bg-gray-400 rounded-t-md text-center">Zusätzlicher Text</p>
+              <textarea class="p-2 rounded-b-md w-full max-h-[10rem]" name="" id="" rows="3"></textarea>
+            </div>
+          </div>
+        </form>
+        <div class="w-full overflow-hidden lg:w-1/2">
+          <p>test</p>
+        </div>
+
+        <!-- statik box -->
+        <div>
+
+        </div>
+      </div>
+    </div>
+    <div class="w-1/2 m-auto">
+      <FullCalendar :options="calendarOptions" />
+    </div>
   </div>
 </template>
 
@@ -15,7 +59,10 @@ export default {
     FullCalendar // make the <FullCalendar> tag available
   },
   data() {
+    let showUntilInput: boolean = true;
+
     return {
+      showUntilInput,
       calendarOptions: {
         plugins: [dayGridPlugin, interactionPlugin],
         initialView: 'dayGridMonth',
