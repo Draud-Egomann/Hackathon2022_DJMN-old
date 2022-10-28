0b7edccc967da489a9e7617b3314620ec3a74be3
Justin <justin@kauz.ch>
2022-10-28 18:45:07 +0200

    Created and imported Navbar

--- Ferienkalender/src/App.vue
+++ Ferienkalender/src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="min-h-full">
+    <Header />
     <router-view></router-view>
   </div>
 </template>
 
 <script setup lang="ts">
+import Header from "./components/Navbar.vue";
 </script>
 
 <style scoped>
---
+++ Ferienkalender/src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <section class="w-full mx-auto">
+      <div class="relative bg-gray-200 p-4">
+        <router-link to="/" class="btn-Header m-2 inline-block text-lg">Kalendar
+        </router-link>
+        <router-link to="/" class="btn-Header m-2 inline-block text-lg">Statistik
+        </router-link>
+        <router-link to="/" class="btn-Header m-2 inline-block text-lg">Anfragen
+        </router-link>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  components: {},
+  setup() { },
+  mounted() { },
+  methods: {},
+});
+</script>
+
+<style lang="css" scoped>
+
+</style>
